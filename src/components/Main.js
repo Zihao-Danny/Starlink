176632215784068da5777b57043b8dbb07e0878f
wang_zihao <zihaow.danny@gmail.com>
2021-01-26 22:35:20 -0800

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <div className='main'>
+                <div className="left-side">
+                    left
+                </div>
+                <div className="right-side">
+                    right
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default Main;
