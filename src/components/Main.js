7b41ab5f95aac2f871e009107d85d151afd0037c
wang_zihao <zihaow.danny@gmail.com>
2021-01-28 22:11:57 -0800

    add SatSettings

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
-
+import axios from 'axios';
+import SatSetting from './SatSettings';
+import SatelliteList from './SatelliteList';
+import {NEARBY_SATELLITE, SAT_API_KEY, STARLINK_CATEGORY} from "../constants";
 class Main extends Component {
     render() {
         return (
             <div className='main'>
                 <div className="left-side">
-                    left
+                    <SatSetting onShow={this.fetchSatellite}/>
+                    <SatelliteList />
                 </div>
                 <div className="right-side">
                     right
@@ -14,6 +18,27 @@ class Main extends Component {
             </div>
         );
     }
+    fetchSatellite= (setting) => {
+        const {latitude, longitude, elevation, altitude} = setting;
+        const url = `/api/${NEARBY_SATELLITE}/${latitude}/${longitude}/${elevation}/${altitude}/${STARLINK_CATEGORY}/&apiKey=${SAT_API_KEY}`;
+
+        this.setState({
+            isLoadingList: true
+        });
+
+        axios.get(url)
+            .then(response => {
+                console.log(response.data)
+                this.setState({
+                    satInfo: response.data,
+                    isLoadingList: false
+                })
+            })
+            .catch(error => {
+                console.log('err in fetch satellite -> ', error);
+            })
+    }
+
 }
 
 export default Main;
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const SAT_API_KEY = "6REV5Y-VSGMQ6-FLH8RQ-4ICE";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
