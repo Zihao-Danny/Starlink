52f81cb573fed68a4d1fac4f86623d3872447a34
wang_zihao <zihaow.danny@gmail.com>
2021-02-03 11:57:45 -0800

    implement SatelliteList and SatSettings

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,21 +3,41 @@ import axios from 'axios';
 import SatSetting from './SatSettings';
 import SatelliteList from './SatelliteList';
 import {NEARBY_SATELLITE, SAT_API_KEY, STARLINK_CATEGORY} from "../constants";
+import WorldMap from "./WordMap"
 class Main extends Component {
+    constructor(){
+        super();
+        this.state = {
+            satInfo: null,
+            settings: null,
+            isLoadingList: false
+        };
+    }
+
     render() {
+        const { satInfo } = this.state;
         return (
             <div className='main'>
                 <div className="left-side">
-                    <SatSetting onShow={this.fetchSatellite}/>
-                    <SatelliteList />
+                    <SatSetting onShow={this.showNearbySatellite}/>
+                    <SatelliteList satInfo={satInfo}
+                                   isLoad={this.state.isLoadingList}
+                    />
                 </div>
                 <div className="right-side">
-                    right
+                    <WorldMap/>
                 </div>
 
             </div>
         );
     }
+    showNearbySatellite = (setting) => {
+        this.setState({
+            settings: setting
+        })
+        this.fetchSatellite(setting);
+    }
+
     fetchSatellite= (setting) => {
         const {latitude, longitude, elevation, altitude} = setting;
         const url = `/api/${NEARBY_SATELLITE}/${latitude}/${longitude}/${elevation}/${altitude}/${STARLINK_CATEGORY}/&apiKey=${SAT_API_KEY}`;
